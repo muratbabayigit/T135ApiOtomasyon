6fa3bba3268a239a7078947718c1d0f90e879041
Babayiğit <babayigit@gmail.com>
2024-07-16 02:24:49 +0300

    merhaba

--- src/test/java/baseUrl/BaseUrlHerokuApp.java
+++ src/test/java/baseUrl/BaseUrlHerokuApp.java
@@ -5,12 +5,13 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class BaseUrlHerokuApp {
-    protected RequestSpecification specHerokuApp;
+
+    protected RequestSpecification specHerOkuApp;
 
     @Before
-    public void setUP(){
-    specHerokuApp=new RequestSpecBuilder()
-                                        .setBaseUri("https://restful-booker.herokuapp.com")
-                                        .build();
+    public  void setUp(){
+        specHerOkuApp=new RequestSpecBuilder()
+                .setBaseUri("https://restful-booker.herokuapp.com")
+                .build();
     }
 }
--- src/test/java/tests/P15_baseUrlHerokuApp.java
+++ src/test/java/tests/P15_baseUrlHerokuApp.java
@@ -2,34 +2,33 @@ package tests;
 
 import baseUrl.BaseUrlHerokuApp;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class P15_baseUrlHerokuApp extends BaseUrlHerokuApp {
+        /*
+         //1-  https://restful-booker.herokuapp.com/booking endpointine
+     //    bir GET request gonderdigimizde
+     //    donen response’un
+     //          status code’unun 200 oldugunu
+     //          ve Response’ta 133 booking oldugunu test edin
 
-
-           /*
-        //1-  https://restful-booker.herokuapp.com/booking endpointine
-    //    bir GET request gonderdigimizde
-    //    donen response’un
-    //          status code’unun 200 oldugunu
-    //          ve Response’ta 133 booking oldugunu test edin
-
-     */
-
+      */
         @Test
-        public void test(){
-            //1-Endpoint hazırlanır
-                specHerokuApp.pathParam("pp1","booking");
-            //2-expBody yok
+        public void test01(){
+                //1-Endpoint hazırlanır
+                specHerOkuApp.pathParam("pp1","booking");
 
-            //3-Response kaydedilir
+                //2-Verilmişse expData hazırlanır.
 
-                Response response=given().spec(specHerokuApp).get("{/pp1}");
+                //3-Dönen Response kaydedilir
+                Response response=given().when().spec(specHerOkuApp).get("/{pp1}");
 
-             //4-Assert işlemleri
-                response.then().assertThat().statusCode(200).
+                //.....Kontrol işlemi
+                response.then().assertThat().statusCode(200).body("bookingid", Matchers.hasItem(133));
+
 
 
 
